8f97bca558285668d590570489a0db37a2c6864f
Pavel.A.Komov <kmvpvl@gmail.com>
2023-05-12 19:43:16 +0300

    fix: body parse

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 8000;
 app.post('/', (req, res)=>{
     
-    console.log(`request: ${req.body}`);
+    console.log(`request: ${JSON.stringify(req.body)}`);
     return res.status(200).json({
         test: "test"
     });
