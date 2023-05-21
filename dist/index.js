38a4c96d7d7494899d08372efba6dd6c8702ee88
kmvpvl <43674119+kmvpvl@users.noreply.github.com>
2023-05-21 20:07:57 +0300

    404 processed

--- dist/index.js
+++ dist/index.js
@@ -8,18 +8,19 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 const PORT = process.env.PORT || 8000;
 app.post('/devicereport', (req, res) => {
-    console.log(`devicereport: ${JSON.stringify(req.body)}`);
+    console.log(`devicereport: client='${req.headers["auth_shome"]}'; body='${JSON.stringify(req.body)}'`);
     return res.status(200).json({
         device: "OK"
     });
 });
 app.post('/initcontroller', (req, res) => {
-    console.log(`controller settings: ${JSON.stringify(req.body)}`);
+    console.log(`controller settings: client='${req.headers["auth_shome"]}'; body='${JSON.stringify(req.body)}'`);
     return res.status(200).json({
         controller: "OK"
     });
 });
 app.use((req, res, next) => {
+    console.log(`Unknown command: '${req.originalUrl}'`);
     return res.status(404).json({});
 });
 app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
