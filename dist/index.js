44e957dd3cb9e5ef67a9d7f84f4c95edb3f25c8a
Pavel.A.Komov <kmvpvl@gmail.com>
2023-05-21 18:13:13 +0300

    added methods

--- dist/index.js
+++ dist/index.js
@@ -7,10 +7,19 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 const PORT = process.env.PORT || 8000;
-app.post('/', (req, res) => {
-    console.log(`request: ${JSON.stringify(req.body)}`);
+app.post('/devicereport', (req, res) => {
+    console.log(`devicereport: ${JSON.stringify(req.body)}`);
     return res.status(200).json({
-        test: "test"
+        device: "OK"
     });
+});
+app.post('/initcontroller', (req, res) => {
+    console.log(`controller settings: ${JSON.stringify(req.body)}`);
+    return res.status(200).json({
+        controller: "OK"
+    });
+});
+app.use((req, res, next) => {
+    return res.status(404).json({});
 });
 app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,23 @@ import express from "express";
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 8000;
-app.post('/', (req, res)=>{
+app.post('/devicereport', (req, res)=>{
     
-    console.log(`request: ${JSON.stringify(req.body)}`);
+    console.log(`devicereport: ${JSON.stringify(req.body)}`);
     return res.status(200).json({
-        test: "test"
+        device: "OK"
     });
 }); 
+
+app.post('/initcontroller', (req, res)=>{
+    console.log(`controller settings: ${JSON.stringify(req.body)}`);
+    return res.status(200).json({
+        controller: "OK"
+    });
+});
+
+app.use((req, res, next) => {
+    return res.status(404).json({});
+})
 
 app.listen(PORT, ()=>console.log(`Now listening on port ${PORT}`));
